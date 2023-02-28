6cff7fe10cc954e9cf7e2b900748eb2342a1386d
manuelnones <manuelnones99@yahoo.com>
2023-02-28 13:08:07 +0100

    correct exercise email

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 let userEmailElement = document.getElementById(`userEmail`);
 let userIdButtonElement = document.getElementById(`userIdButton`);
 let listEmails = [`manueln@email.com`, `adriana@email.com`, `simone@email.com`, `liviov@email.com`, `patrick@email.com`];
-let valideEmailElement = getElementById(`valideEmail`)
+let valideEmailElement = document.getElementById(`valideEmail`);
 
 userIdButtonElement.addEventListener(`click`, function () {
     let emailFound = false;
@@ -12,8 +12,8 @@ userIdButtonElement.addEventListener(`click`, function () {
         };
     };
     if (emailFound) {
-        document.writeln("Email corretta")
+        valideEmailElement.innerText = `Email correta, accesso consentito!`
     } else {
-        document.writeln("Email non valida!")
+        valideEmailElement.innerText = `Accesso negato, email non trovata!`
     }
 });
